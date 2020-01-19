1ad8661a5bb4a523b33899de718e511c795b5530
Hunter V <hmvitous@yahoo.com>
2020-01-19 17:19:17 +0100

    Creating movie year list

---
+++ movie_years.rb
@@ -0,0 +1,10 @@
+movies = { "The Shawshank Redemption" => 1994, 
+    "Titanic" => 1997, 
+    "The Godfather" => 1972, 
+    "The Wizard of Oz" => 1939, 
+    "Psycho" => 1960 }
+
+movies.each do |title, year| 
+    puts  "#{year}"
+end
+           
