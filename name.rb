c41d82ed057d5be142406a5bc7fe6ae800362056
Hunter V <hmvitous@yahoo.com>
2020-01-19 18:09:24 +0100

    Adding name input and name repeating 10 times

---
+++ name.rb
@@ -0,0 +1,8 @@
+puts "Hello, what is your first name?"
+firstName = gets.chomp
+puts"And your last name?"
+lastName = gets.chomp
+puts "Welcome #{firstName} #{lastName}"
+10.times do
+puts  "#{firstName} #{lastName}"
+end
