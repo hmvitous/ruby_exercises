2a67fdf27f3bbfc851c0ae30b808b1b4b34db4e7
Hunter V <hmvitous@yahoo.com>
2020-01-19 18:27:36 +0100

    Made simple program asking age and telling future ages

---
+++ age.rb
@@ -0,0 +1,14 @@
+puts "How old are you?"
+age = gets.chomp.to_i
+sum1 = age + 10
+sum2 = age + 20
+sum3 = age + 30
+sum4 = age + 40
+puts "In 10 years you will be: 
+#{sum1} "
+puts "In 20 years you will be: 
+#{sum2} "
+puts "In 30 years you will be: 
+#{sum3} "
+puts "In 40 years you will be: 
+#{sum4} "
